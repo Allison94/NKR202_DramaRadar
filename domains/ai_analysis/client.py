4870baaa1044deefb6cb8abee47059b22576a517
Allison-mac <kiwi2711@hotmail.com.tw>
2026-07-25 18:23:36 +0000

    feat: :speech_balloon: 新增APIError錯誤訊息抓取

--- domains/ai_analysis/client.py
+++ domains/ai_analysis/client.py
@@ -13,6 +13,9 @@ logger = logging.getLogger(__name__)
 def genai_client():
     try:
         yield genai.Client(api_key=settings.gemini_api_key) # 在pipeline.py用完才關閉
+    except APIError as e:
+        logger.error(f"[Error: genai_client]狀態碼: {e.code}, 訊息: {e.message}")
+        raise e
     except Exception as e:
-        logger.exception("[Error: genai_client]gemini連線發生錯誤")
+        logger.exception("[Error: genai_client] gemini連線發生錯誤")
         raise e
