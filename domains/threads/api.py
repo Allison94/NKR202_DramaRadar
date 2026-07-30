d24d079be628566b31d3612cd846d5f727cfdbc7
Sam <sam10183133@gmail.com>
2026-07-30 17:47:14 +0000

    回傳資料存資料庫

--- domains/threads/api.py
+++ domains/threads/api.py
@@ -2,13 +2,13 @@ import datetime
 
 import requests
 from shared.config import settings
-# 抓ＰＯＳＴ
-# curl -i -X GET \
-#  "https://graph.threads.net/v1.0/18063333437725637?fields=id%2Cmedia_product_type%2Cmedia_type%2Cmedia_url%2Cpermalink%2Cowner%2Cusername%2Ctext%2Ctopic_tag%2Ctimestamp%2Cshortcode%2Cthumbnail_url%2Cchildren%2Cis_quote_post&access_token=<放ＴＯＫＥＮ>"
+from db.database import engine,metadata
+from db.shared_tables import execution_log
+from domains.threads.models import threads_log
+from sqlalchemy import insert,select
 
+user_id = "38348086084790846" #!組長說這個要改到env
 
-user_id = "38348086084790846"
-# post 
 url = f"https://graph.threads.com/v1.0"
 create_container_endpoint = f"/{user_id}/threads"
 header = {
@@ -33,8 +33,19 @@ if rs_data:
 
 
     params = {
-        "fields": "id,username,text,media_type,media_url,permalink,timestamp"
+        "fields": "id,text,media_type,media_url,permalink,timestamp"
     }   
     if publish_id:
         rs = requests.get(url=f"{url}/{publish_id}",headers=header,params=params)
-        print(rs.json())
+        publish_data = rs.json()
+        published_id = publish_data["id"]
+        print(publish_data)
+        # {'id': '17888281206670059', 'username': 'dramarada202', 'text': 'This post is test By API. 2026-07-30 15:19:56.300646', 'media_type': 'TEXT_POST', 'permalink': 'https://www.threads.com/@dramarada202/post/Dba_yM7gUrYQrf9EVPlzGupf2FWDzxx9FZTyKc0', 'timestamp': '2026-07-30T15:19:59+0000'}
+
+        if published_id:
+            metadata.create_all(engine)
+            with engine.begin() as conn:
+                stmt = insert(threads_log)
+                rs = conn.execute(stmt,publish_data)
+                if rs.rowcount > 0:
+                    print("儲存成功")
