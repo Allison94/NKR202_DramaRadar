f6f27c225035d24965e5a9468907917913c4e841
Allison <kiwi2711@hotmail.com.tw>
2026-08-11 18:11:31 +0000

    feat: :construction: 設定dag

--- domains/threads/dag.py
+++ domains/threads/dag.py
@@ -1,13 +1,18 @@
 from datetime import datetime,timedelta
 from airflow.sdk import task,dag
 from domains.threads.api import threads_run
-import logging
-from airflow.exceptions import AirflowException
+import logging,sys,os
+from domains.threads.token_access import refresh_threads_token
 
+current_dir = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_dir)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+    
 logger = logging.getLogger(__name__)
 
 args = {
-    "reties":2,
+    "retries":2,
     "retry_delay":timedelta(minutes=10)
 }
 
@@ -17,6 +22,7 @@ args = {
     start_date=datetime(2026,1,1),
     schedule="0 19 * * *",
     catchup=False,
+    max_active_runs=1,
     default_args=args,
     tags=["threads","daily"]
 )
@@ -25,8 +31,11 @@ def threads_dags():
 
     @task
     def threads_daily_post():
+        
         logger.info(f"[INFO: threads_daily_post] Start")
-        threads_run()
+        refresh_key = refresh_threads_token()
+        if refresh_key:
+            threads_run()
         logger.info(f"[INFO: threads_daily_post] End")
 
     threads_daily_post()
